97bb264db2dbd06505e321e3901ac4f8a75ef823
AndyLPK247 <AndyLPK247@gmail.com>
2019-05-13 22:03:04 -0400

    more Actor 'knows' tests

--- tests/test_actor.py
+++ tests/test_actor.py
@@ -7,9 +7,10 @@ This module contains unit tests for the Actor class.
 # ------------------------------------------------------------------------------
 
 import pytest
+import sys
 
 from collections import OrderedDict
-from screenplay.actor import Actor
+from screenplay.actor import Actor, UnknowableArgumentError
 from screenplay.pattern import *
 
 
@@ -45,6 +46,81 @@ def be(actual, value):
 @condition
 def contain(actual, value):
   return value in actual
+
+
+@interaction
+def do_it(task):
+  return f"{task} is done"
+
+
+@interaction
+def whip_it_good():
+  pass
+
+
+@question
+def some_question():
+  pass
+
+
+@saying
+def try_things(actor, name):
+  def try_it():
+    return f"tried {name}"
+  return try_it
+
+
+@saying
+def shout(actor, name):
+  def out_loud(words):
+    return words.upper()
+  return out_loud
+
+
+@task
+def some_task():
+  pass
+
+
+# ------------------------------------------------------------------------------
+# Pattern Assertion Functions
+# ------------------------------------------------------------------------------
+
+def assert_abilities(actor):
+  assert len(actor.abilities) == 2
+  assert actor.abilities['be_cool'] == be_cool
+  assert actor.abilities['go_super_saiyan'] == go_super_saiyan
+
+
+def assert_conditions(actor):
+  assert len(actor.conditions) == 2
+  assert actor.conditions['be'] == be
+  assert actor.conditions['contain'] == contain
+
+
+def assert_interactions(actor, include_tq=False):
+  if include_tq:
+    assert len(actor.interactions) == 4
+    assert actor.interactions['some_task'] == some_task
+    assert actor.interactions['some_question'] == some_question
+  else:
+    assert len(actor.interactions) == 2
+
+  assert actor.interactions['do_it'] == do_it
+  assert actor.interactions['whip_it_good'] == whip_it_good
+
+
+def assert_sayings(actor):
+  assert len(actor.sayings) == 2
+  assert actor.sayings['try_things'] == try_things
+  assert actor.sayings['shout'] == shout
+
+
+def assert_all_functions(actor, include_tq=False):
+  assert_abilities(actor)
+  assert_conditions(actor)
+  assert_interactions(actor, include_tq=include_tq)
+  assert_sayings(actor)
 
 
 # ------------------------------------------------------------------------------
@@ -121,21 +197,23 @@ def test_actor_knows_an_ability(actor):
 def test_actor_knows_multiple_abilities_with_one_call_each(actor):
   actor.knows(be_cool)
   actor.knows(go_super_saiyan)
-  assert len(actor.abilities) == 2
-  assert actor.abilities['be_cool'] == be_cool
-  assert actor.abilities['go_super_saiyan'] == go_super_saiyan
+  assert_abilities(actor)
 
 
 def test_actor_knows_multiple_abilities_with_one_call_for_all(actor):
   actor.knows(be_cool, go_super_saiyan)
-  assert len(actor.abilities) == 2
-  assert actor.abilities['be_cool'] == be_cool
-  assert actor.abilities['go_super_saiyan'] == go_super_saiyan
+  assert_abilities(actor)
 
 
 def test_actor_knows_abilities_in_order(actor):
   actor.knows(go_super_saiyan, be_cool)
   assert list(actor.abilities.keys()) == ['go_super_saiyan', 'be_cool']
+
+
+def test_actor_knows_a_duplicate_ability(actor):
+  actor.knows(be_cool, go_super_saiyan)
+  actor.knows(be_cool)
+  assert_abilities(actor)
 
 
 # ------------------------------------------------------------------------------
@@ -151,16 +229,12 @@ def test_actor_knows_a_condition(actor):
 def test_actor_knows_multiple_conditions_with_one_call_each(actor):
   actor.knows(be)
   actor.knows(contain)
-  assert len(actor.conditions) == 2
-  assert actor.conditions['be'] == be
-  assert actor.conditions['contain'] == contain
+  assert_conditions(actor)
 
 
 def test_actor_knows_multiple_conditions_with_one_call_for_all(actor):
   actor.knows(be, contain)
-  assert len(actor.conditions) == 2
-  assert actor.conditions['be'] == be
-  assert actor.conditions['contain'] == contain
+  assert_conditions(actor)
 
 
 def test_actor_knows_conditions_in_order(actor):
@@ -168,15 +242,155 @@ def test_actor_knows_conditions_in_order(actor):
   assert list(actor.conditions.keys()) == ['contain', 'be']
 
 
+def test_actor_knows_a_duplicate_condition(actor):
+  actor.knows(be, contain)
+  actor.knows(be)
+  assert_conditions(actor)
+
+
+# ------------------------------------------------------------------------------
+# Tests for Knowing Interactions
+# ------------------------------------------------------------------------------
+
+def test_actor_knows_an_interaction(actor):
+  actor.knows(do_it)
+  assert len(actor.interactions) == 1
+  assert actor.interactions['do_it'] == do_it
+
+
+def test_actor_knows_a_task(actor):
+  actor.knows(some_task)
+  assert len(actor.interactions) == 1
+  assert actor.interactions['some_task'] == some_task
+
+
+def test_actor_knows_a_question(actor):
+  actor.knows(some_question)
+  assert len(actor.interactions) == 1
+  assert actor.interactions['some_question'] == some_question
+
+
+def test_actor_knows_multiple_interactions_with_one_call_each(actor):
+  actor.knows(do_it)
+  actor.knows(whip_it_good)
+  assert_interactions(actor)
+
+
+def test_actor_knows_multiple_interactions_with_one_call_for_all(actor):
+  actor.knows(do_it, whip_it_good)
+  assert_interactions(actor)
+
+
+def test_actor_knows_interactions_in_order(actor):
+  actor.knows(whip_it_good, do_it)
+  assert list(actor.interactions.keys()) == ['whip_it_good', 'do_it']
+
+
+def test_actor_knows_a_duplicate_interaction(actor):
+  actor.knows(do_it, whip_it_good)
+  actor.knows(do_it)
+  assert_interactions(actor)
+
+
+# ------------------------------------------------------------------------------
+# Tests for Knowing Sayings
+# ------------------------------------------------------------------------------
+
+def test_actor_knows_a_saying(actor):
+  actor.knows(try_things)
+  assert len(actor.sayings) == 1
+  assert actor.sayings['try_things'] == try_things
+
+
+def test_actor_knows_multiple_sayings_with_one_call_each(actor):
+  actor.knows(try_things)
+  actor.knows(shout)
+  assert_sayings(actor)
+
+
+def test_actor_knows_multiple_sayings_with_one_call_for_all(actor):
+  actor.knows(try_things, shout)
+  assert_sayings(actor)
+
+
+def test_actor_knows_sayings_in_order(actor):
+  actor.knows(shout, try_things)
+  assert list(actor.sayings.keys()) == ['shout', 'try_things']
+
+
+def test_actor_knows_a_duplicate_saying(actor):
+  actor.knows(try_things, shout)
+  actor.knows(try_things)
+  assert_sayings(actor)
+
+
+# ------------------------------------------------------------------------------
+# Tests for Knowing Multiple Types
+# ------------------------------------------------------------------------------
+
+def test_actor_knows_multiple_types_with_one_call(actor):
+  actor.knows(be_cool, go_super_saiyan, be, contain, do_it, whip_it_good, try_things, shout)
+  assert_all_functions(actor)
+
+
+# ------------------------------------------------------------------------------
+# Tests for Knowing Modules
+# ------------------------------------------------------------------------------
+
+def test_actor_knows_module(actor):
+  actor.knows(sys.modules[__name__])
+  assert_all_functions(actor, include_tq=True)
+
+
+# ------------------------------------------------------------------------------
+# Tests for Knowing Other Actors
+# ------------------------------------------------------------------------------
+
+def test_actor_knows_another_actor(actor):
+  other = Actor()
+  other.knows(be_cool, go_super_saiyan, be, contain, do_it, whip_it_good, try_things, shout)
+  actor.knows(other)
+  assert_all_functions(actor)
+
+
+# ------------------------------------------------------------------------------
+# Tests for Knowing Non-Screenplay Items
+# ------------------------------------------------------------------------------
+
+def test_actor_cannot_know_an_arbitrary_function(actor):
+  def func():
+    pass
+
+  with pytest.raises(UnknowableArgumentError):
+    actor.knows(func)
+
+
+def test_actor_cannot_know_an_arbitrary_object(actor):
+  obj = object()
+  with pytest.raises(UnknowableArgumentError):
+    actor.knows(obj)
+
+
+def test_actor_cannot_know_an_arbitrary_list(actor):
+  stuff = list((1, 2, 3))
+  with pytest.raises(UnknowableArgumentError):
+    actor.knows(stuff)
+
+
+def test_actor_cannot_know_an_arbitrary_set(actor):
+  stuff = set((1, 2, 3))
+  with pytest.raises(UnknowableArgumentError):
+    actor.knows(stuff)
+
+
+def test_actor_cannot_know_an_arbitrary_dict(actor):
+  stuff = dict(a=1, b=2, c=3)
+  with pytest.raises(UnknowableArgumentError):
+    actor.knows(stuff)
+
+
 # Test Actor
 
-# know interactions
-# know sayings
-# know actor
-# know module
-# know multiple
-# know duplicate
-# know none of the above
 # can ability without args
 # can ability with args
 # can unknown ability
